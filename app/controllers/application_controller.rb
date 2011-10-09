0a8709e42c2a90bdb9b5a9e4b745f883707ce721
Jonathan Spooner <jspooner@gmail.com>
2011-10-08 18:28:02 -0700

    Fixed after login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,12 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
   
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      request.env['omniauth.origin'] || root_url
+    else
+      super
+    end
+  end
   
 end
