173162f058b2c9a547e363a84bf67e8032dcd738
Jonathan Spooner <jspooner@gmail.com>
2011-10-26 21:10:57 -0700

    Change from bing to google to make the search api work again.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,7 @@
 # config/initializers/geocoder.rb
 
 # geocoding service (see below for supported options): :yahoo :bing
-Geocoder::Configuration.lookup = :bing
+Geocoder::Configuration.lookup = :google
 
 # # to use an API key:
 Geocoder::Configuration.api_key = "AoLaowV5I4Valvvk23m9g5uMPS2PGEEfvyhnTRWIFJeo0La1gZWm2It1EjUjXAVZ"
