7f7d32bf32956f22414a33f34e41bc78d0c5efd0
Jonathan Spooner <jspooner@gmail.com>
2011-10-30 14:39:41 -0700

    Added a location search box.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,14 @@
 class SearchController < ApplicationController
   def index
-    @query     = params[:q]
-    @locations = Location.limit(25).where("city LIKE ? or state LIKE ? or postal LIKE ? or name LIKE ?", "%#{@query}%", "%#{@query}%", "%#{@query}%", "%#{@query}%")
+    @query          = params[:q]
+    @query_location = params[:l]
+    
+    @locations = Location.limit(25)
+    @locations = @locations.near(@query_location)
+    @locations = @locations.where("city LIKE ? or state LIKE ? or postal LIKE ? or name LIKE ?", "%#{@query}%", "%#{@query}%", "%#{@query}%", "%#{@query}%")
+
+
+
+    # @locations = @locations.near(params[:l]) if params[:l]
   end
 end
