017db931e1781c575961d5b910a9fbd936e2318a
Jonathan Spooner <jspooner@gmail.com>
2011-10-30 17:13:28 -0700

    Removed near search option

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,11 +4,6 @@ class SearchController < ApplicationController
     @query_location = params[:l]
     
     @locations = Location.limit(25)
-    @locations = @locations.near(@query_location)
     @locations = @locations.where("city LIKE ? or state LIKE ? or postal LIKE ? or name LIKE ?", "%#{@query}%", "%#{@query}%", "%#{@query}%", "%#{@query}%")
-
-
-
-    # @locations = @locations.near(params[:l]) if params[:l]
   end
 end
