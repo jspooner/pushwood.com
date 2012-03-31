fb1ef64a1958f1836fbb29d732ec6015f9755719
Jonathan Spooner <jspooner@gmail.com>
2012-03-31 13:20:07 -0700

    Fixed url host in image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,7 @@ require 'net/https'
 require 'uri'
 class Image < ActiveRecord::Base
   include Rails.application.routes.url_helpers
-  Rails.application.routes.default_url_options[:host] = 'http://pushwood.com'
+  Rails.application.routes.default_url_options[:host] = 'http://pushwood.com/'
   # SERIALIZED ATTRIBUTES 
   # CONSTANTS 
   # SCOPES
@@ -48,16 +48,16 @@ class Image < ActiveRecord::Base
   def post_to_facebook
     logger.info { "post_to_facebook-------------------------- #{share_on_fb}" }
     return if share_on_fb != "true" and (!user.nil? or !user.facebook_access_token.nil? or !location.nil?)
-
-    logger.info { "POSTING... post_to_facebook--------------------------" }
-    me = FbGraph::User.me(user.facebook_access_token)
-    me.feed!(
+    post = {
       :message => "I posted a photo of #{location.name}",
       :picture => "#{Rails.application.routes.default_url_options[:host]}#{img.url(:thumb)}",
       :link => location_url(location),
       :name => location.name,
       :description => location.ios_description
-    )
+    }
+    logger.info { "POSTING... post_to_facebook--------------------------  #{post}" }
+    me = FbGraph::User.me(user.facebook_access_token)
+    me.feed!(post)
     
     # curl -F 'access_token=AAAC4m77AVg8BAAvKse9MQ00jYmaeslpYbTYgZBDcG8jn4elTrTgZAeZCF2sKZBIa2wgixqwwy34B5fCRX22WJk2wf9yrCeUZD' \
     #      -F 'skate_park=http://pushwood.com/locations/3255-homage-brooklyn' \
