da9bc07f9bd6a698ea5586c463bbd514705dc3de
Jonathan Spooner <jspooner@gmail.com>
2012-03-31 12:47:28 -0700

    Fixed url host in image.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,6 @@ Woodhack::Application.configure do
   config.action_dispatch.best_standards_support = :builtin
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-
   
   # Facbook app for Pushwood.dev
   config.facebook_id      = '265888350098080'
