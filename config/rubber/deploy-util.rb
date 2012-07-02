1969e4495ea438ffb6f5f54d191d9ffcd77440c2
Jonathan Spooner <jspooner@gmail.com>
2012-07-02 07:40:15 -0700

    Added redis to rubber

--- config/rubber/deploy-util.rb
+++ config/rubber/deploy-util.rb
@@ -88,15 +88,31 @@ namespace :rubber do
     end    
     
     desc <<-DESC
-      push local files
+      push local mysql files
     DESC
-    task :push_local_files do
+    task :push_local_mysql do
       upload "/Users/jonathanspooner/Downloads/pushwoodcom.2012-07-01T01-10-04.gz", "/home/ubuntu/pushwoodcom.2012-07-01T01-10-04.gz"
       run "gunzip /home/ubuntu/pushwoodcom.2012-07-01T01-10-04.gz"
       rsudo "mysql -h 127.0.0.1 -u pushwood -p pushwood_production < ./pushwoodcom.2012-07-01T01-10-04"
       
       upload "/Users/jonathanspooner/Downloads/redis_state.rdb", "/home/ubuntu/redis_state.rdb"
     end
+
+    desc <<-DESC
+      push local redis files
+    DESC
+    task :push_local_mysql do
+      upload "/Users/jonathanspooner/Downloads/redis_state.rdb", "/home/ubuntu/redis_state.rdb"
+    end
+
+    desc <<-DESC
+      push local images
+    DESC
+    task :push_local_mysql do
+      upload "/Users/jonathanspooner/Downloads/redis_state.rdb", "/mnt/pushwood-"
+    end
+    
+    
     
   end
 end
