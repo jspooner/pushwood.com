ea67014c394f79cfa9819308b187d620ead02e82
Jonathan Spooner <jspooner@gmail.com>
2012-07-04 09:00:57 -0700

    Added code to push images

--- config/rubber/deploy-transfer-ey.rb
+++ config/rubber/deploy-transfer-ey.rb
@@ -8,11 +8,10 @@ namespace :rubber do
         push local mysql files
       DESC
       task :push_local_mysql do
-        upload "/Users/jonathanspooner/Downloads/pushwoodcom.2012-07-01T01-10-04.gz", "/home/ubuntu/pushwoodcom.2012-07-01T01-10-04.gz"
-        run "gunzip /home/ubuntu/pushwoodcom.2012-07-01T01-10-04.gz"
-        rsudo "mysql -h 127.0.0.1 -u pushwood -p pushwood_production < ./pushwoodcom.2012-07-01T01-10-04"
-  
-        upload "/Users/jonathanspooner/Downloads/redis_state.rdb", "/home/ubuntu/redis_state.rdb"
+        sql_filename = "pushwoodcom.2012-07-03T01-10-03"
+        # upload "/Users/jonathanspooner/Downloads/#{sql_filename}.gz", "/home/ubuntu/#{sql_filename}.gz"
+        # run "gunzip /home/ubuntu/#{sql_filename}.gz"
+        rsudo "mysql -h 127.0.0.1 -u pushwood -p pushwood_production < /home/ubuntu/#{sql_filename}"
       end
 
       desc <<-DESC
@@ -26,7 +25,10 @@ namespace :rubber do
         push local images
       DESC
       task :push_local_images do
-        upload "engineyard/system/uploads/", "/mnt/pushwood-production/shared/system/uploads/"
+        # tar cvzf uploads.tar.gz uploads
+        # upload "engineyard/system/uploads.tar.gz", "/mnt/pushwood-production/shared/system/uploads.tar.gz"
+        rsudo "tar xvzf #{shared_path}/system/uploads.tar.gz #{shared_path}/system/uploads"
+        # rsudo "rm tarbar.tar.gz"
       end
       
     end
