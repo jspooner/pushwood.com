d291842f09280ce1199257c5c5f239d429849e4b
Jonathan Spooner <jspooner@gmail.com>
2012-07-15 09:16:05 -0700

    Added storage

--- config/rubber/deploy-transfer-ey.rb
+++ config/rubber/deploy-transfer-ey.rb
@@ -8,27 +8,39 @@ namespace :rubber do
         push local mysql files
       DESC
       task :push_local_mysql do
-        sql_filename = "pushwoodcom.2012-07-03T01-10-03"
-        # upload "/Users/jonathanspooner/Downloads/#{sql_filename}.gz", "/home/ubuntu/#{sql_filename}.gz"
-        # run "gunzip /home/ubuntu/#{sql_filename}.gz"
-        rsudo "mysql -h 127.0.0.1 -u pushwood -p pushwood_production < /home/ubuntu/#{sql_filename}"
+        sql_filename = "pushwoodcom.2012-07-14T01-10-02"
+        upload "/Users/jonathanspooner/Downloads/#{sql_filename}.gz", "/home/ubuntu/#{sql_filename}.gz"
+        run "gunzip /home/ubuntu/#{sql_filename}.gz"
+        env = rubber_cfg.environment.bind("mysql_master", "web01")
+        rsudo "mysql --host=web01.pushwood.com --user=pushwood --password=#{env.db_pass} pushwood_production < /home/ubuntu/#{sql_filename}"
+      end
+
+      desc <<-DESC
+        push local redis files
+      DESC
+      task :pull_redis do
+        system("scp -r deploy@ec2-107-20-242-3.compute-1.amazonaws.com:/db/redis/redis_state.rdb db/redis_state.rdb")
       end
 
       desc <<-DESC
         push local redis files
       DESC
       task :push_local_redis do
-        upload "/Users/jonathanspooner/Downloads/redis_state.rdb", "/mnt/redis/redis_state.rdb"
+        upload "db/redis_state.rdb", "/mnt/redis/redis_state.rdb"
       end
-
+      
       desc <<-DESC
         push local images
       DESC
       task :push_local_images do
-        # tar cvzf uploads.tar.gz uploads
-        # upload "engineyard/system/uploads.tar.gz", "/mnt/pushwood-production/shared/system/uploads.tar.gz"
+        system("tar cvzf ./public/system.tar.gz ./public/system")
+        upload "./public/system.tar.gz", "#{shared_path}/system/uploads.tar.gz"
         rsudo "tar xvzf #{shared_path}/system/uploads.tar.gz #{shared_path}/system/uploads"
         # rsudo "rm tarbar.tar.gz"
+      end
+      
+      task :pulldown_images do
+        system("scp -r deploy@ec2-107-20-242-3.compute-1.amazonaws.com:/data/pushwoodcom/shared/system/ ./public/")
       end
       
     end
