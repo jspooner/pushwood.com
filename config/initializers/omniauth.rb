da2842a3d2cc307b228f0a11e02f266f487049fb
Jonathan Spooner <jspooner@gmail.com>
2012-07-17 07:38:15 -0700

    Hardcoding full_url in omni_auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -13,3 +13,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
   use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'yahoo', :identifier => 'yahoo.com'
 end
+
+if Rails.env == "production"
+  OmniAuth.config.full_host = 'http://pushwood.com'
+end
